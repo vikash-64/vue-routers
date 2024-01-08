71fa7f84209878037d7ecbc2091fa66336d6f06d
viksh-64 <vikash400001@gmail.com>
2024-01-08 17:49:48 +0530

    .

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import About from '@/views/About.vue';
+import Home from '@/views/Home.vue';
+import Brazil from '@/views/Brazil.vue';
+import Hawaii from '@/views/Hawaii.vue';
+import Jamaica from '@/views/Jamaica.vue';
+import Panama from '@/views/Panama.vue';
+
+const routes =  [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/About', name: 'About', component: About },
+    {path:'/brazil' , name:'brazil' , component:Brazil},
+    {path:'/hawaii' , name:'hawaii' , component:Hawaii},
+    {path:'/jamaica' , name:'jamaica' , component:Jamaica},
+    {path:'/panama' , name:'panama' , component:Panama},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+}
+);
+
+
+
+export default router
---
+++ src/views/Brazil.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="brazil">
+           <h1>Brazil</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
